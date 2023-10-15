b27393a577a1751c9d01a3938b6f45ad305b1cc0
Grip <nsk540010@gmail.com>
2023-10-15 22:14:27 +0700

    созданы функции проверки корректности ввода, В функции заполнения булевского поля Возможно нужно внести изменения(добавить a и b ) чтобы не писать еще одну отдельную функцию. Как я думаю, практически все готово для расстановки кораблей для одного человека, НО НУЖНО ( дописать функцию заполнения булевского поля) ИЛИ написать ЕЩЕ ОДНУ.

--- sea_battle/sea_battle.cpp
+++ sea_battle/sea_battle.cpp
@@ -8,6 +8,33 @@ void field(bool boolean_field[10][10], int x, int y)
 {
     boolean_field[x - 1][y - 1] = true;
 }
+
+// функция корректности ввода A
+int correct_of_input_A(int a)
+{
+    while (a < 1 || a >10)
+    {
+        std::cout << "incorrect input!!!" << std::endl;
+        std::cout << "enter coordinates from 1 to 10:  ";
+        std::cin >> a;
+
+    }
+    return a;
+}
+
+// функция корректности ввода B
+int correct_of_input_B(int b)
+{
+    while (b < 1 || b >10)
+    {
+        std::cout << "incorrect input!!!" << std::endl;
+        std::cout << "enter coordinates from 1 to 10:  ";
+        std::cin >> b;
+
+    }
+    return b;
+}
+
 // функция корректности ввода X
 int correct_of_input_X(int x)
 {
@@ -20,6 +47,7 @@ int correct_of_input_X(int x)
     }
     return x;
 }
+
 // функция корректности ввода Y
 int correct_of_input_Y(int y)
 {
@@ -33,29 +61,95 @@ int correct_of_input_Y(int y)
     return y;
 }
 
+// функция проверки корректности расстановки кораблей
+bool correction(int a, int b, int x, int y,bool boolean_field[10][10])
+{
+    // проверка на горизонтальное расположение корабля
+    if (a - x == 0 && b - y <  0)
+    {
+        for (; b <= y; b++)
+        {
+            if (boolean_field[a][b] == true)
+            {
+                return false;
+            }  
+        }
+    }
+
+    if (a - x == 0 && b - y > 0)
+    {
+        for (; b >= y; b--)
+        {
+            if (boolean_field[a][b] == true)
+            {
+                return false;
+            }
+        }
+    }
+    // Проверка на вертикальное расположение
+    if (a - x < 0 && b - y == 0)
+    {
+        for (; a <= x; a++)
+        {
+            if (boolean_field[a][b] == true)
+            {
+                return false;
+            }
+        }
+    }
+
+    if (a - x > 0 && b - y == 0)
+    {
+        for (; a >= x; a--)
+        {
+            if (boolean_field[a][b] == true)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 
 int main()
 {
     bool boolean_field[10][10]{};  //создаем булевское поле
-    int x, y;
-    std::cout << "enter x ,  y  ";
+    int a=0, b=0; // координаты начала корабля
+    int x=0, y=0; // координаты конца корабля
 
-    std::cin >> x;
-    std::cin >> y;
+    // Ввод четырех однопалубных кораблей
+    for(int i = 0; i < 4 ; i++)
+    {
+        std::cout << "enter the coordinates of 1 deck ship using a space or enter: " << std::endl;
 
-    correct_of_input_X(x);
-    correct_of_input_Y(y);
-    field(boolean_field, x, y);
-    std::cin >> x;
-    std::cin >> y;
+        std::cin >> x >> y;
+       
 
-    correct_of_input_X(x);
-    correct_of_input_Y(y);
-    field(boolean_field, x, y);
+        correct_of_input_X(x); // проверка корректности ввода x;
+        correct_of_input_Y(y); // проверка корректности ввода y;
+        field(boolean_field, x, y);
+    }
+    // Ввод трех двухпалубных кораблей
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << "enter the coordinates of 2 deck ship using a space or enter: " << std::endl;
+
+        std::cin >> x >> y;
+        std::cin >> a >> b;
+        correct_of_input_X(x);
+        correct_of_input_Y(y);
+        correct_of_input_A(a);
+        correct_of_input_B(b);
+
+    }
+   
 
     std::cout << boolean_field[0][0];
+    std::cout << boolean_field[1][1];
+    std::cout << boolean_field[2][2];
     std::cout << boolean_field[3][3];
+
 
     return 0;
 }
